94fcd4e89c5f2755c881ea7080120c8898003bf7
its-me-anubhav <anubhavshakya543@gmail.com>
2025-01-26 22:53:55 +0530

    add new words

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -6,7 +6,7 @@ int main () {
 	printf("Enter the number of terms");
 	scanf("%d", &n);
 	// print the first two terms t1 and t2
-	printf("Fibonacci series:%d,%d",t1,t2);
+	printf("Fibonacci serries:%d,%d",t1,t2);
 	// print 3rd to next term
 	for(i=3;i<=n;++i) {
 		printf("%d",nextTerm);
